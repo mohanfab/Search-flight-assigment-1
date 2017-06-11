99411573fc6070501e5fc0bbad0322c8491753ee
mohanfab <pardeshi.moha@gmail.com>
2017-06-11 21:34:48 +0530

    changes for grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,3 @@
-/**
- * Created by parrikaa on 11/03/16.
- */
 
 // our wrapper function (required by grunt and its plugins)
 // all configuration goes inside this function
@@ -12,12 +9,6 @@ module.exports = function(grunt) {
         // get the configuration info from package.json
         pkg: grunt.file.readJSON('package.json'),
 
-        // all of our configuration will go here
-        karma: {
-            unit: {
-                configFile: 'karma.conf.js'
-            }
-        },
         serve: {
             options: {
                 port: 8080,
@@ -32,7 +23,6 @@ module.exports = function(grunt) {
     // LOAD GRUNT PLUGINS ========================================================
     // we can only load these if they are in our package.json
     // make sure you have run npm install so our app can find these
-    grunt.loadNpmTasks('grunt-karma');
     grunt.loadNpmTasks('grunt-serve');
     grunt.loadNpmTasks('grunt-npm-install');
     grunt.loadNpmTasks('grunt-bower-install');
--- README.md
+++ README.md
@@ -1,13 +1,11 @@
 <h3>Introduction</h3>
 
 <h3>How to install</h3>
-<h5>Execute the following commands in sequence
-incluse project relative path in command e.g C:\Users\dell\Downloads\Search-flight-assigment-1-master
-</h5>
+<h5>Execute the following commands in sequence</h5>
 <ol>
     <li><code>npm install</code></li>
     <li><code>bower install</code></li>
-    <li><code>grunt</code> To start the local server http://localhost:8080/app/index.html</li>
+    <li><code>grunt</code>          To start the local server http://localhost:8080/app/index.html</li>
 </ol>
 
 <h3>Technical features</h3>
